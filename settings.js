e063bfcf7279ba124416417756a03e917f2fefff
savindu8 <119857048+savindu8@users.noreply.github.com>
2023-04-21 16:11:09 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -4,30 +4,30 @@ const chalk = require('chalk')
 
 global.botname = "Queen Anya" 
 global.botLanguage = "default" // 𝗗𝗲𝗳𝗮𝘂𝗹𝘁 || 𝗘𝗻𝗴𝗹𝗶𝘀𝗵 || 𝗛𝗶𝗻𝗱𝗶 || 𝗔𝗿𝗮𝗯𝗶𝗰 \\
-global.author = "𝙋𝙄𝙆𝘼"  
+global.author = "BUVI"  
 global.packname = "𝙌𝙐𝙀𝙀𝙉✯𝘼𝙉𝙔𝘼✯𝘽𝙊𝙏"  
-global.myweb = "https://github.com/PikaBotz" // You can put any link 
-global.waterMark = "×͜× King ✘ Pika™ ✓" // Your own watermark 
-global.headerText = "Anya by Pika" // text for header of menu         
-global.DocumentTitle = "MASTER⚔️PIKA" 
+global.myweb = "https://github.com/savindu8" // You can put any link 
+global.waterMark = "×͜× King ✘ Buvi™ ✓" // Your own watermark 
+global.headerText = "Anya by Buvi" // text for header of menu         
+global.DocumentTitle = "MASTER⚔️𝙱𝚄𝚅𝙸" 
 
 
-global.ownername = "𝕻𝖎𝖐𝖆♰𝕶𝖚𝖓"   // Owner name //        
-global.ownernumber = ['916900904828'] 
-global.socialm = "𝗜𝗻𝘀𝘁𝗮 : 3.69_pika" // 𝗜𝗻𝘀𝘁𝗮 : || 𝗚𝗶𝘁𝗵𝘂𝗯 : || 𝗙𝗮𝗰𝗲𝗯𝗼𝗼𝗸 : \\
+global.ownername = "𝕭𝖚𝖛𝖎♰𝕶𝖚𝖓"   // Owner name //        
+global.ownernumber = ['94764766973'] 
+global.socialm = "𝗜𝗻𝘀𝘁𝗮 : 3.69_Buvi" // 𝗜𝗻𝘀𝘁𝗮 : || 𝗚𝗶𝘁𝗵𝘂𝗯 : || 𝗙𝗮𝗰𝗲𝗯𝗼𝗼𝗸 : \\
 global.ytname = "YT: Ytname"   // Yt chanel name //
 global.continent = "Asia" // your continent name 
-global.region = "India" // your country name
-global.state = "Assam" // your state name
-global.district = "Dibrugarh" // your district name
-global.timezone = "Asia/Kolkata" // search on google if you don't know the timezone of your country //
-global.instagramId = "3.69_pika" // Your insta ID
-global.email = "example@exmaple.com" // put your email here
+global.region = "Sri Lanka" // your country name
+global.state = "Sabaragamuva" // your state name
+global.district = "Ratnapure" // your district name
+global.timezone = "Asia/Sri Lanka" // search on google if you don't know the timezone of your country //
+global.instagramId = "3.69_BUVi" // Your insta ID
+global.email = "buvindunisal@gmail.com" // put your email here
 
 
 //------------ 🌟 Premium Users 🌟-------------\\
-global.premium = ['916900******','917355******']  // available soon
-global.sudo = ['916900******','917355******']  // Co - Owners //
+global.premium = ['94764766973','917355******']  // available soon
+global.sudo = ['94764766973,'917355******']  // Co - Owners //
 
 global.prefa = ['-']
 
